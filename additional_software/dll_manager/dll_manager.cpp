884b9c42e1fbcd83597df5c60e493cc7f75bd78f
Pavel <itztoru@vk.com>
2023-08-15 18:22:23 +0600

    Added new funcs to dll_manager.cpp

--- additional_software/dll_manager/dll_manager.cpp
+++ additional_software/dll_manager/dll_manager.cpp
@@ -1,6 +1,25 @@
 #include <filesystem>
 #include <iostream>
+#include <vector>
+
+namespace fs = std::filesystem;
+
+std::vector<fs::path> FindAllDLL(const fs::path& path){
+
+}
+
+bool IsCorrectPath(const std::string& path){
+
+}
+
+bool CopyDLLs(const std::string& target, const std::vector<fs::path>& form){
+
+}
 
 int main(int argc, char* argv[]){
+
+#ifndef WIN32
+    std::cerr << "Only for Windows platfom!" << std::ndl;
+#endif
 
 }

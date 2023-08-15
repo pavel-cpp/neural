aab8b32978b9be4d0f30f1c825f7e89a2f8ca6f3
Pavel <itztoru@vk.com>
2023-08-15 18:42:02 +0600

    Added Functional to FindAllDLL

--- additional_software/dll_manager/dll_manager.cpp
+++ additional_software/dll_manager/dll_manager.cpp
@@ -4,15 +4,21 @@
 
 namespace fs = std::filesystem;
 
-std::vector<fs::path> FindAllDLL(const fs::path& path){
+typedef std::vector<fs::path> PathList;
 
+PathList FindAllDLL(const fs::path& path){
+    PathList files;
+    for(const auto& file : fs::directory_iterator(path)){
+        files.push_back(file);
+    }
+    return files;
 }
 
 bool IsCorrectPath(const std::string& path){
 
 }
 
-bool CopyDLLs(const std::string& target, const std::vector<fs::path>& form){
+bool CopyDLLs(const fs::path& target, const std::vector<fs::path>& form){
 
 }
 
@@ -21,5 +27,14 @@ int main(int argc, char* argv[]){
 #ifndef WIN32
     std::cerr << "Only for Windows platfom!" << std::ndl;
 #endif
+
+    for(int i = 0; i < argc; ++i){
+        std::cout << i << argv[i] << std::endl;
+        fs::path path = argv[i];
+        if(path.has_filename()){
+            path = path.parent_path();
+        }
+        PathList files = FindAllDLL(path);
+    }
 
 }

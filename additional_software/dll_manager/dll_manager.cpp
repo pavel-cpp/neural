7f7c543fc2275a409b44d1406f1cd453658c94f6
Pavel <toruchan174@gmail.com>
2023-08-19 02:23:48 +0600

    Refctoring
    filesystem_interface splitted to 2 new files

--- additional_software/dll_manager/dll_manager.cpp
+++ additional_software/dll_manager/dll_manager.cpp
@@ -1,9 +1,4 @@
-#include <filesystem>
-#include <regex>
-#include <vector>
-#include <iostream>
-
-#include "CLI/include/ProgressBar.h"
+#include "filesystem_interface.hpp"
 
 namespace fs = std::filesystem;
 
@@ -16,7 +11,6 @@ using std::endl;
 using std::wstring;
 using std::string;
 using std::vector;
-
 
 typedef vector<fs::path> Paths;
 typedef vector<string> Dependents;
@@ -70,52 +64,6 @@ Dependents GetDependentDlls(const wstring &executable_file_path) {
     }
 
     return dependent_dlls;
-}
-
-Paths GetFiles(const fs::path &path, const string &extension) {
-    Paths files;
-    for (const auto &file: fs::directory_iterator(path)) {
-        if (file.path().extension() == extension) {
-            files.push_back(file);
-        }
-    }
-    return files;
-}
-
-string GetDirectory(const fs::path &path) {
-    if (path.has_filename() && !is_directory(path)) {
-        return path.parent_path().string();
-    }
-    return path.string();
-}
-
-bool IsCorrectPath(const string &path) {
-    regex expr(R"([A-Z]:\\((\w+\\)*))");
-    smatch match;
-    regex_search(path, match, expr);
-    return !match.empty();
-}
-
-bool IsDependent(const string &path, const Dependents &dependents) {
-    return std::any_of(dependents.begin(), dependents.end(), [path](const string &dependent) {
-        return path.find(dependent) != string::npos;
-    });
-}
-
-void CopyDLLs(const fs::path &dest, const Paths &dlls, const Dependents &dependents) {
-    CLI::ProgressBar progress_bar(0, dlls.size());
-    for (const auto &file: dlls) {
-        if (!IsDependent(file.string(), dependents)) continue;
-        try {
-            fs::copy(file, dest.string() + "\\" + file.filename().string(), fs::copy_options::overwrite_existing);
-        } catch (...) {
-            wcout << progress_bar.Next(L'\x2593');
-            Sleep(100);
-            continue;
-        }
-        wcout << progress_bar.Next();
-        Sleep(100);
-    }
 }
 
 int main(int argc, char *argv[]) {
